7dfdd44abcc444044fdeac22d3cb7f9e4d4f2799
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2023-03-31 15:51:06 +0200

    modify native code with app json

--- android/app/src/main/java/com/test/bundle/MainActivity.java
+++ android/app/src/main/java/com/test/bundle/MainActivity.java
@@ -1,4 +1,4 @@
-package com.placeholder.appid;
+package com.test.bundle;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/test/bundle/MainApplication.java
+++ android/app/src/main/java/com/test/bundle/MainApplication.java
@@ -1,4 +1,4 @@
-package com.placeholder.appid;
+package com.test.bundle;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
